f9c71a68e6befdd242071f2a48d28699696c9e57
Mohd Ariz Ansari <arizansari80@gmail.com>
2017-08-11 07:34:15 +0530

    Almost Final Confirm Registration

--- JS/AJAX/makeAccount.js
+++ JS/AJAX/makeAccount.js
@@ -4,6 +4,11 @@ getAccountBtn.addEventListener('click',getAccountNumber);
 
 var ajaxHttp=new XMLHttpRequest();
 
+var openAccBtn=document.getElementById('openAccountBtn');
+openAccBtn.disabled=true;
+openAccBtn.style.cursor="not-allowed";
+openAccBtn.style.background="grey";
+
 function getAccountNumber(){
 	var refIDSend=document.getElementById('referenceID').value;
 
@@ -18,6 +23,20 @@ function getAccountNumber(){
 			if(myObj.status.localeCompare('Success')==0){
 				console.log(myObj);
 				accn.value=myObj.accN;
+				document.getElementById('permanentAccountNumber').value=myObj.accN;
+				document.getElementById('branchName').value=myObj.BName;
+				if(myObj.BCode.charCodeAt(0)==48)
+					myObj.BCode=myObj.BCode.substr(1);
+				document.getElementById('branchCode').value=myObj.BCode;
+				document.getElementById('ifscCode').value=myObj.IFSC;
+				var toAct=document.getElementById('toActivateField').getElementsByTagName('input');
+				for (var i = 0; i < toAct.length; i++) {
+					toAct[i].className="commonInput compulsory";
+					toAct[i].disabled=false;
+				}
+				openAccBtn.disabled=false;
+				openAccBtn.style.cursor="";
+				openAccBtn.style.background="#5ea559";
 			}
 			else{
 				if(myObj.accN!=0){
